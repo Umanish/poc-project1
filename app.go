cc4873c048aad505f1d5525f7898a323a9422003
Manish Kumar <manish.kumar@tokopedia.com>
2023-04-04 13:21:20 +0530

    first commit

---
+++ app.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := "8080"
+
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("Hello World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
